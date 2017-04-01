5cd84fef4961ac127d15d8234cca0a5892890d38
tonyOP1985 <toliverpaull@gmail.com>
2017-04-01 18:46:06 -0400

    Update AlphabetPositions.js

--- AlpabetPosition/AlphabetPositions.js
+++ AlpabetPosition/AlphabetPositions.js
@@ -1,5 +1,5 @@
 // Takes a string and returns every letter's position in the alphabet.
-// Non-alphabet characters and numbers are ignores.
+// Non-alphabet characters and numbers are ignored.
 // alphabet_position("The sunset sets at twelve o' clock.")
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (As a string.)
 
